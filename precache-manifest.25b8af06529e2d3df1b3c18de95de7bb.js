66e9f190b0435f30905cea8998d18fbad617c9ce
jeevi-mouli <jeev2588@gmail.com>
2019-11-15 00:56:03 +0530

    Updates

---
+++ precache-manifest.25b8af06529e2d3df1b3c18de95de7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c1107f007114ff68dfb14d4b2d9f55",
+    "url": "/krishna-transports/index.html"
+  },
+  {
+    "revision": "e750d0c8d4b0da605399",
+    "url": "/krishna-transports/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "feedcc6aaf74b32dcf0d",
+    "url": "/krishna-transports/static/js/2.40c823c5.chunk.js"
+  },
+  {
+    "revision": "e750d0c8d4b0da605399",
+    "url": "/krishna-transports/static/js/main.2cffa987.chunk.js"
+  },
+  {
+    "revision": "9918f6c0a00248d1b693",
+    "url": "/krishna-transports/static/js/runtime-main.0249c15f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/krishna-transports/static/media/logo.25bf045c.svg"
+  }
+]);
